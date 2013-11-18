f70343bbf19611eb1d512db58afe33128f1b2833
jwzhai <jwzhai.job2011@gmail.com>
2013-11-18 10:09:32 +0800

    Change layout construction of activity_main.

--- MyApplicationExampleProject/MyApplicationExample/src/main/java/com/leon/example/MainActivity.java
+++ MyApplicationExampleProject/MyApplicationExample/src/main/java/com/leon/example/MainActivity.java
@@ -10,12 +10,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.view.Window;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
